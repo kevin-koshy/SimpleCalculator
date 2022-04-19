2214ad3e624260db312182233ca88536747f1ebf
Kevin Koshy <kevin999koshy@gmail.com>
2022-04-19 10:29:27 +0530

    Final Commit

--- calculator.js
+++ calculator.js
@@ -10,7 +10,12 @@ app.listen(3000, function() {
 
 app.post("/", function(req, res){
     console.log(req.body);
-    res.send("Thanks for posting.");
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var num3 = Number(req.body.num3);
+    var result = num1 + num2 + num3;
+
+    res.send("Result of calculation is "+result);
 })
 
 
